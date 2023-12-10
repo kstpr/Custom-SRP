f48f2822445fb271571aa3448fde9011ebf9af7a
Kalin Presnakov <kpresnakov@gmail.com>
2023-12-10 15:23:42 +0200

    Initial commit

---
+++ Assets/CustomRP/CameraRenderer.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer {
+
+    ScriptableRenderContext context;
+    Camera camera;
+
+    const string bufferName = "Render Camera";
+
+    CommandBuffer buffer = new CommandBuffer {
+        name = bufferName
+    };
+
+    private CullingResults cullingResults;
+
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+    public void Render(ScriptableRenderContext context, Camera camera) {
+        this.context = context;
+        this.camera = camera;
+
+        if (!Cull()) return;
+
+        Setup();
+        DrawVisibleGeometry();
+        Submit();
+    }
+
+    private void Setup() {
+        context.SetupCameraProperties(camera);
+        buffer.ClearRenderTarget(true, true, Color.clear);
+        buffer.BeginSample(bufferName);
+        ExecuteBuffer();
+    }
+
+    void DrawVisibleGeometry() {
+        var sortingSettings = new SortingSettings(camera) {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+        var filteringSettings = new FilteringSettings(RenderQueueRange.all);
+
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+
+        context.DrawSkybox(camera);
+    }
+
+    void Submit() {
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
+        context.Submit();
+    }
+
+    private void ExecuteBuffer() {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    private bool Cull() {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters parameters)) {
+            cullingResults = context.Cull(ref parameters);
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/CustomRP/CustomRenderPipelineAsset.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/Custom Render Pipeline")]
+public class CustomRenderPipelineAsset : RenderPipelineAsset {
+    protected override RenderPipeline CreatePipeline() {
+        return new CustomRenderPipeline();
+    }
+}
